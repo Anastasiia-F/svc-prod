3f2089455cb0e21039c47fdac120133786dffb84
Anastasiia <afesenk@gmail.com>
2019-02-19 08:37:41 +0200

    Coment mongodb setup

--- api-server/app/app.js
+++ api-server/app/app.js
@@ -32,7 +32,7 @@ const port = process.env.PORT || 3000;
 /**
  * Connect to MongoDB.
  */
-mongoose.set('useNewUrlParser', true);
+/*mongoose.set('useNewUrlParser', true);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 mongoose.connect(`${process.env.MONGODB_URI}`);
@@ -40,7 +40,7 @@ mongoose.connection.on('error', (err) => {
   debug(err);
   debug('%s MongoDB connection error. Please make sure MongoDB is running.', red('✗'));
   process.exit();
-});
+});*/
 
 
 /**
